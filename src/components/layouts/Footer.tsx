a9640ab409b1a49649da239c018569c27f86761c
Joseph Alberto <47499991+joseph-alberto@users.noreply.github.com>
2023-09-02 23:20:23 +0700

    Creating Footer Section

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,18 +1,34 @@
-import React from "react"
+import Link from "next/link"
+import { FaGithub, FaLinkedin } from "react-icons/fa6"
 
-type Props = {}
+const Footer = () => {
 
-const Footer = (props: Props) => {
+  const socialLinks = [
+    {
+      icon: (<FaGithub size={18} className='hover:text-teal-500 hover:transition-colors transition-colors'/>),
+      url: "https://github.com/joseph-alberto",
+    },
+    {
+      icon: (<FaLinkedin size={19} className='hover:text-teal-500 hover:transition-colors transition-colors'/>),
+      url: "https://linkedin.com/in/joseph-alberto",
+    },
+  ]
+
   return (
-    <footer className='container flex flex-col mx-auto py-12 text-gray-600 text-sm'>
+    <footer className='container flex flex-col gap-4 mx-auto py-12 text-gray-600 text-sm'>
+      <hr />
       <div>
-        <span className=''>©</span> 2023 Joseph Web&apos;s.
+        <span className=''>©</span> 2023{" "}
+        <Link href={''}>Joseph Web&apos;s.</Link>
       </div>
       <div>
-          <ul className="flex gap-4">
-            <li>A</li>
-            <li>B</li>
-          </ul>
+        <ul className='flex'>
+          {socialLinks.map((social,i)=>(
+            <li className="pr-2 pl-2 border-r" key={i}>
+              <Link target="_blank" href={social.url}>{social.icon}</Link>
+            </li>
+          ))}
+        </ul>
       </div>
     </footer>
   )
