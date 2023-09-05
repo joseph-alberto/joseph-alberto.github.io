2425a316ca8a7841173d56c3c549a5795c6ddbbd
Joseph Alberto <47499991+joseph-alberto@users.noreply.github.com>
2023-09-05 15:34:22 +0700

    add more portfolio data

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -20,7 +20,7 @@ const Page = (props: Props) => {
         Portfolio
       </h1>
       <p className='text-sm'>
-        Over here you can see some of the work I&apos;ve created so far. Some project is made for my self and some of it&aposo;s for the company or someone who ask me to create the application for their needs, please take a look *eye*.
+        Over here you can see some of the work I&apos;ve created so far. Some project is made for my self and some of it&apos;s for the company or someone who ask me to create the application for their needs, please take a look *eye*.
       </p>
       <div className='mt-8'>
         <PortfolioContainer data={portfolios} />
