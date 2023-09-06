6500966a6e3aad5b729678f6d2bb0a960b1f1ab8
Joseph Alberto <47499991+joseph-alberto@users.noreply.github.com>
2023-09-06 16:45:08 +0700

    add xs responsive breakpoint

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,14 @@ const config: Config = {
         },
       },
     },
+    screens: {
+      xs: "384px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
   },
   plugins: [],
 }
